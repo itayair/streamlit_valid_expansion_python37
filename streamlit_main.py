4bd8feece2167b32c15393b6cfd4fbb091eb5069
itayair <iy24592@gmail.com>
2022-03-03 17:06:55 +0200

    add all files

--- streamlit_main.py
+++ streamlit_main.py
@@ -18,6 +18,13 @@ import streamlit_valid_expansion
 #         pip._internal.main(['install', package])
 #
 # # Example
-if __name__ == '__main__':
-    streamlit_valid_expansion.main()
+# if __name__ == '__main__':
+#     streamlit_valid_expansion.main()
     # install("https://storage.googleapis.com/en_ud_model/en_ud_model_sm-2.0.0.tar.gz")
+
+import sys
+from streamlit import cli as stcli
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", "streamlit_valid_expansion.py"]
+    sys.exit(stcli.main())
