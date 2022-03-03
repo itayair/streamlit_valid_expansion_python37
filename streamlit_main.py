d2f01ef428b0c0fc06234a34a3862e89da0ece75
itayair <iy24592@gmail.com>
2022-03-03 17:09:51 +0200

    add all files

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,7 +4,7 @@ import streamlit as st
 import subprocess
 import sys
 import pip
-import streamlit_valid_expansion
+# import streamlit_valid_expansion
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "https://storage.googleapis.com/en_ud_model"
 #                                                                "/en_ud_model_sm-2.0.0.tar.gz"])
 # subprocess.run([f"{sys.executable}", "streamlit_valid_expansion.py"])
