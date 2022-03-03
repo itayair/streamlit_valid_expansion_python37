55bcb9e185f252cf74d60de7604a65363c62a7b9
itayair <iy24592@gmail.com>
2022-03-03 16:01:23 +0200

    add all files

--- streamlit_main.py
+++ streamlit_main.py
@@ -5,8 +5,8 @@ import subprocess
 import sys
 import os
 cwd = os.getcwd()
-print(cwd)
+print("itay yair", cwd)
 
-subprocess.run(["./Scripts/python.exe", "streamlit_valid_expansion.py"])
+# subprocess.run(["./Scripts/python.exe", "streamlit_valid_expansion.py"])
 
 # p = subprocess.Popen([f"{sys.executable} -m streamlit_valid_expansion.py"],shell=True)
