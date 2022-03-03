ef92ddb2499583b62ba4a2d2e5e24d6334124b1b
itayair <iy24592@gmail.com>
2022-03-03 15:40:01 +0200

    add all files

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,7 +4,7 @@ import streamlit as st
 import subprocess
 import sys
 
-subprocess.run(["/home/appuser/venv/bin/python", "streamlit_valid_expansion.py"])
+# subprocess.run(["/home/appuser/venv/bin/python", "streamlit_valid_expansion.py"])
 
-# p = subprocess.Popen([f"{sys.executable} -m streamlit_valid_expansion.py"], shell=True)
+p = subprocess.Popen([f"{sys.executable} -m streamlit_valid_expansion.py"], shell=True)
 # p.run()
